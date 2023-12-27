d132a36d4cd3a1fbd1554587102ad6e5b34b32d7
anas1hallak <anas1hallak@gmail.com>
2023-12-27 17:43:14 +0300

    no commit

--- database/migrations/2023_12_21_112711_create_matches_table.php
+++ database/migrations/2023_12_21_112711_create_matches_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('matches', function (Blueprint $table) {
+        Schema::create('matches', function (Blueprint $table) { 
             $table->increments('id');
             $table->date('date');
             $table->time('time');
